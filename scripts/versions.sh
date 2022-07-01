51d01f71b6481e8f780e3cf8b659eb2e16dbf4da
Saleem Farid <davidhill0173@gmail.com>
2022-07-01 15:22:32 +0100

    truthy

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Set up the versions to be used
-coreth_version=${CORETH_VERSION:-'v1.0.5'}
+coreth_version=${CORETH_VERSION:-'v1.0.6'}
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'v0.3.0'}
+avalanche_version=${AVALANCHE_VERSION:-'v1.0.0'}
