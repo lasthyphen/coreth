caca2e70d856465daf3bfbd7da14cd0768aad010
Dijets Ltd <info@dijets.io>
2022-07-04 11:40:42 +0100

    configs and flags

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Set up the versions to be used
-coreth_version=${CORETH_VERSION:-'v0.9.8'}
+coreth_version=${CORETH_VERSION:-'v0.9.9'}
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'v1.2.2'}
+avalanche_version=${AVALANCHE_VERSION:-'v1.2.3'}
